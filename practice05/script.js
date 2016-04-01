449fb9bb9885ea7337bdbecd29e37c29c0be0681
Betsy Gottesman <eightamps@gmail.com>
2016-04-01 12:16:36 -0700

    practice problem 5

---
+++ practice05/script.js
@@ -0,0 +1,13 @@
+function count(){
+    for(i = 1; i < 101; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("fizzbuzz " + i);
+        } else if(i % 5 === 0) {
+            console.log("buzz " + i);
+        } else if(i % 3 === 0){
+            console.log("fizz " + i);
+        } else{console.log(i);}
+    }
+}
+
+count();
