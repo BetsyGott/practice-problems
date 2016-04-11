e991dd879fe35fb8220af0aa52db3cce268168d0
Betsy Gottesman <eightamps@gmail.com>
2016-04-11 15:33:01 -0700

    practice 26 Betsy Gottesman

--- practice26/answer.js
+++ practice26/answer.js
@@ -1,1 +1,18 @@
-answer.js
+var haystack = ["New York", "Los Angeles", "Tokyo", "DC", "Paris"];
+var needle = ["Chicago", "Detroit", "Tokyo", "Paris", "Amsterdam"];
+//needs to find tokyo and paris
+
+function searchArray(searchThis, findThese){
+    var results = [];
+    for(i=0; i < findThese.length; i++){
+// loop through each value in the needle array 
+        if(searchThis.indexOf(findThese[i])!==-1){
+
+//if it finds the needle value
+            results.push(findThese[i]);
+        }
+    }
+    console.log(results);
+}
+
+searchArray(haystack, needle);
